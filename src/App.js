db77bb52be252e5511aec4a3e6154e64ee6e2e9f
stephanietako <gabeshine@live.fr>
2023-05-31 18:04:11 +0200

    starting project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Page404 from "./components/Page404";
+import Maintenance from "./pages/Maintenance";
 
 // Styles
 import "./App.scss";
@@ -10,7 +11,11 @@ const App = () => (
   <div className="App">
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {JSON.parse(process.env.REACT_APP_MODE_MAINTENANCE) ? (
+          <Route path="/" element={<Maintenance />} />
+        ) : (
+          <Route path="/" element={<Home />} />
+        )}
         <Route path="*" element={<Page404 />} />
       </Routes>
     </Router>
