f367127441d95219ad0adab7bad161c970a922c6
stephanietako <gabeshine@live.fr>
2023-06-06 02:10:07 +0200

    work in progress

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,7 @@
 import { useState, useRef, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 // Assets
-import logo from "../../assets/logo/takodev-logo-black.png";
+import logo from "../../assets/logo/takodev-logo-white.png";
 import menu from "../../assets/icon/menu.svg";
 import cross from "../../assets/icon/cross.svg";
 // Components
@@ -10,13 +10,9 @@ import Links from "../Links";
 import styles from "./styles.module.scss";
 
 const Navbar = () => {
-  // The error "Uncaught TypeError: Cannot read properties of null (reading 'offsetHeight')"
-  //In this case, it is possible that you are trying to access the offsetHeight property of a DOM element that has not yet been rendered, or that is not present in the DOM.
-  // Lets fix it: One way to do this is to wrap the code that accesses the DOM element in a useEffect hook, and to make sure that the hook only runs after the element has been rendered.
   const navbarElement = useRef(null);
   let navigationHeight = 0;
   useEffect(() => {
-    // This effect will only run after the navbar element has been rendered
     // eslint-disable-next-line
     navigationHeight = navbarElement.current.offsetHeight;
     console.info("Navbar height:", navbarElement.current.offsetHeight);
@@ -24,12 +20,28 @@ const Navbar = () => {
       "--scroll-padding",
       navigationHeight
     );
+
+    // Handle resize
+    const handleResize = () => {
+      if (window.innerWidth >= 1200) {
+        setIsMobile(false);
+      } else {
+        setIsMobile(true);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, [navbarElement]);
 
   // navbar color state change managment
   const [fix, setFix] = useState(false);
   const setFixed = () => {
-    if (window.scrollY >= 142) {
+    console.log(window.scrollY);
+    if (window.scrollY >= 111) {
       setFix(true);
     } else {
       setFix(false);
@@ -37,11 +49,40 @@ const Navbar = () => {
   };
   window.addEventListener("scroll", setFixed);
 
-  //menu state change management
+  // menu state change management
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => setIsOpen(!isOpen);
 
+  // Mobile menu detection
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const handleInitialResize = () => {
+      if (window.innerWidth >= 1200) {
+        setIsMobile(false);
+      } else {
+        setIsMobile(true);
+      }
+    };
+
+    handleInitialResize();
+
+    // Cleanup function
+    return () => {
+      window.removeEventListener("resize", handleInitialResize);
+    };
+  }, []);
+
+  //////////
   // Main navbar menu
+  const links = [
+    { name: "Accueil", target: "#welcome" },
+    { name: "A propos", target: "#about" },
+    { name: "Showroom", target: "#showroom" },
+    { name: "Produits", target: "", dropdown: true },
+    { name: "Projets", target: "#projects" },
+    { name: "Contact", target: "#contact" },
+  ];
+  // Main navbar menu for mobile
   const linksMobile = [
     { name: "Accueil", target: "#welcome" },
     { name: "A propos", target: "#about" },
@@ -70,22 +111,15 @@ const Navbar = () => {
       className={fix ? `${styles.navbar} ${styles.fixed}` : `${styles.navbar}`}
     >
       <a href="#welcome">
-        <img
-          className={styles.__logo}
-          src={logo}
-          alt="B&B Italia Saint-Tropez"
-        />
+        <img className={styles.__logo} src={logo} alt="Tako dev logo" />
       </a>
-
-      <div className={styles.__nav_burger_menu}>
-        <button
-          className={styles.__summ}
-          onClick={toggleMenu}
-          onMouseEnter={toggleMenu}
-        >
-          <img src={isOpen ? cross : menu} alt={isOpen ? "Menu" : "Cross"} />
-        </button>
-        {isOpen && (
+      {/* MOBILE MENU */}
+      <div
+        className={
+          isMobile ? `${styles.__nav_burger_menu} ` : `${styles.__nav_menu}`
+        }
+      >
+        {isOpen && isMobile && (
           <div>
             {linksMobile.map((link, index) => {
               return (
@@ -100,7 +134,36 @@ const Navbar = () => {
             })}
           </div>
         )}
+        <button
+          className={styles.__summ}
+          onClick={toggleMenu}
+          onMouseEnter={toggleMenu}
+        >
+          <img src={isOpen ? cross : menu} alt={isOpen ? "Menu" : "Cross"} />
+        </button>
       </div>
+      {/* DESKTOP MENU */}
+      {!isMobile && (
+        <ul className={styles.__nav_menu}>
+          {links.map((link, index) => {
+            let dropdownPosition = 0;
+            const ddList = links.filter((lnk) => lnk.dropdown === true);
+            ddList.map((lnk, index) => {
+              if (link.name === lnk.name) dropdownPosition = index;
+              return null;
+            });
+            return (
+              <Links
+                key={uuidv4()}
+                link={link}
+                sublinks={sublinks}
+                dropdownPosition={dropdownPosition}
+                onClick={toggleMenu}
+              />
+            );
+          })}
+        </ul>
+      )}
     </nav>
   );
 };
