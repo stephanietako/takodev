e1814b194aeb5220dcdcdba9ee86d3e405bfd662
stephanietako <gabeshine@live.fr>
2023-09-12 13:34:57 +0200

    wip: rework & refacto

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,10 +1,9 @@
 // Styles
 import styles from "./styles.module.scss";
 // Imports
-// import logo from "../../assets/logo/takodev-logo-white.png";
 import GlitchImg from "../GlitchImg";
 
-const About = ({ title, text, textmobile }) => {
+const About = ({ title, subtitle, text, textmobile }) => {
   return (
     <div className={styles.about} id="about">
       <div className={styles.__container}>
@@ -17,8 +16,11 @@ const About = ({ title, text, textmobile }) => {
           <div className={`${styles.box} ${styles.__text} `}>
             <div className={styles.__regular_text}>
               <span>
-                <h1>{title}</h1>
+                <h1> {title}</h1>
                 <br></br>
+              </span>
+              <span>
+                <h2>{subtitle}</h2>
               </span>
               <p>{text}</p>
             </div>
