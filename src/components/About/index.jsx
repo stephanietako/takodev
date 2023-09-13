bcb8fa33ea9e625d19bd2b9d2a23acb0e5028d6a
stephanietako <gabeshine@live.fr>
2023-09-13 20:16:53 +0200

    wip: add scroll to text on about components

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,3 +1,4 @@
+import React, { useRef, useEffect } from "react";
 // Styles
 import styles from "./styles.module.scss";
 // Imports
@@ -5,8 +6,29 @@ import GlitchImg from "../GlitchImg";
 // Assets
 import chevronleft from "../../assets/icon/chevronleft.svg";
 import chevronright from "../../assets/icon/chevronright.svg";
+///////////////
+const scrollToRef = (ref) => {
+  if (ref && ref.current) {
+    ref.current.scrollIntoView({
+      behavior: "smooth", // Pour un défilement fluide
+      block: "start", // Défilement pour afficher le haut de l'élément
+    });
+  }
+};
 
+/////////////////
 const About = ({ title, subtitle, text, textmobile }) => {
+  ////////////////////
+  const scrollToTxt = useRef(null);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      scrollToRef(scrollToTxt);
+    }, 2000); // Déclenche le défilement après 2 secondes
+
+    return () => clearTimeout(timer); // Nettoie le timer si le composant est démonté avant l'échéance
+  }, []);
+  console.log("MYREF !!!!", scrollToTxt.current);
+  /////////////////////////
   return (
     <div className={styles.about} id="about">
       <div className={styles.__container}>
@@ -37,7 +59,11 @@ const About = ({ title, subtitle, text, textmobile }) => {
               </span>
               <p>{text}</p>
             </div>
-            <div className={styles.__text_mobile} id={styles.textAboutMobile}>
+            <div
+              ref={scrollToTxt}
+              className={styles.__text_mobile}
+              // id={styles.textAboutMobile}
+            >
               <span>
                 <img
                   className={styles.__icon_left}
