cf6811499a85ff7dca83b2c9a7a3a8f0dcfdcebc
stephanietako <gabeshine@live.fr>
2023-09-19 13:28:09 +0200

    wip: add feature tentacle anime

---
+++ src/components/Tentacles/index.jsx
@@ -0,0 +1,43 @@
+// Styles
+import styles from "./styles.module.scss";
+
+const Tentacles = () => {
+  const renderDivs = (n) => {
+    if (n > 0) {
+      return (
+        // <div className={styles.__renderTentacles} key={n}>
+        <div
+          className={`${styles.__renderTentacles} ${styles.__tentacles} `}
+          key={n}
+        >
+          {renderDivs(n - 1)}
+        </div>
+      ); // Rendu récursif
+    }
+    return null; // Retourne null lorsque n atteint 0
+  };
+
+  const tentacles = 10;
+  let n = tentacles; // Déclaration de n avec let pour pouvoir le modifier
+
+  const tentacleElements = []; // Je stocke les éléments de tentacules
+
+  // Boucle while pour générer les éléments de tentacules
+  while (n > 0) {
+    tentacleElements.push(
+      <div className={`_${tentacles - n + 1} ${styles.tentacle}`} key={n}>
+        {renderDivs(55)}
+        {/* Appel de la fonction pour générer des div imbriqués */}
+      </div>
+    );
+    n--; // Décrémentation de n à chaque itération
+  }
+
+  return (
+    <div className={styles.octopus}>
+      <div className={styles.container}>{tentacleElements}</div>{" "}
+    </div>
+  );
+};
+
+export default Tentacles;
