c33b0b572383ad43eb8cdd7cca466b8e5dd3c4e0
stephanietako <gabeshine@live.fr>
2023-09-21 17:54:39 +0200

    add   src/components/Footer/

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import styles from "./styles.module.scss";
+import logo from "../../assets/logo/takodev-logo-white.png";
+
+const Footer = () => {
+  const date = new Date();
+  const currentYear = date.getFullYear();
+
+  return (
+    <div className={styles.footer}>
+      <footer className={styles.__container}>
+        <ul>
+          <li>
+            <a href="#welcome">
+              <img
+                className={styles.__logo}
+                src={logo}
+                alt="B&B Italia Saint-Tropez"
+              />
+            </a>
+          </li>
+          <li>
+            <Link to="terms">Mentions légales</Link>
+          </li>
+          <li>
+            <div className={styles.__copyright}>
+              &#169; Copyright {currentYear} | Tako Dev |
+            </div>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
