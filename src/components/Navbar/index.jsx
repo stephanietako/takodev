db983d36fbd1ce3198bd3447ba99f6b168d0749f
stephanietako <gabeshine@live.fr>
2023-09-22 17:39:50 +0200

    wip: add about & rework

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,30 +1,26 @@
 import { useState, useRef, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
-// Assets
 import logo from "../../assets/logo/takodev-logo-white.png";
 import menu from "../../assets/icon/menu.svg";
 import cross from "../../assets/icon/cross.svg";
-// Components
 import Links from "../Links";
-// Styles
 import styles from "./styles.module.scss";
 
 const Navbar = () => {
   const navbarElement = useRef(null);
-  let navigationHeight = 0;
+  const navigationHeight = useRef(0); // Utilisez useRef ici
 
   useEffect(() => {
-    // eslint-disable-next-line
-    navigationHeight = navbarElement.current.offsetHeight;
+    navigationHeight.current = navbarElement.current.offsetHeight;
     console.info("Navbar height:", navbarElement.current.offsetHeight);
     navbarElement.current.style.setProperty(
       "--scroll-padding",
-      navigationHeight
+      navigationHeight.current
     );
   }, [navbarElement]);
 
-  //navbar color state change managment
   const [fix, setFix] = useState(false);
+
   const setFixed = () => {
     if (window.scrollY >= 114) {
       setFix(true);
@@ -32,22 +28,17 @@ const Navbar = () => {
       setFix(false);
     }
   };
+
   window.addEventListener("scroll", setFixed);
 
-  // menu state change management
   const [isOpen, setIsOpen] = useState(false);
+
   const toggleMenu = () => setIsOpen(!isOpen);
 
-  // Main navbar menu
   const links = [
     { name: "Accueil", target: "#welcome" },
     { name: "Projets", target: "#projects" },
-    { name: "Contact", target: "#contact" },
-  ];
-  // Main navbar menu for mobile
-  const linksMobile = [
-    { name: "Accueil", target: "#welcome" },
-    { name: "Projets", target: "#projects" },
+    { name: "A Propos", target: "#about" },
     { name: "Contact", target: "#contact" },
   ];
 
@@ -59,13 +50,11 @@ const Navbar = () => {
       <a href="#welcome">
         <img className={styles.__logo} src={logo} alt="Tako dev logo" />
       </a>
-      {/* DESKTOP MENU */}
       <div className={styles.__nav_menu}>
-        {links.map((link) => {
-          return <Links key={uuidv4()} link={link} onClick={toggleMenu} />;
-        })}
+        {links.map((link) => (
+          <Links key={uuidv4()} link={link} onClick={toggleMenu} />
+        ))}
       </div>
-      {/* MOBILE MENU */}
       <div className={styles.__nav_burger_menu}>
         <button
           className={styles.__summ}
@@ -76,9 +65,9 @@ const Navbar = () => {
         </button>
         {isOpen && (
           <div className={styles.__menu_mobile}>
-            {linksMobile.map((link) => {
-              return <Links key={uuidv4()} link={link} onClick={toggleMenu} />;
-            })}
+            {links.map((link) => (
+              <Links key={uuidv4()} link={link} onClick={toggleMenu} />
+            ))}
           </div>
         )}
       </div>
