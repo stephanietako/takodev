1361a8fae12c023468ed8ff597f5bedfa6f43cf4
stephanietako <gabeshine@live.fr>
2023-09-23 15:24:40 +0200

    rework & refacto

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
-// Styles
 import styles from "./styles.module.scss";
-// Assets
 import crossImage from "../../assets/icon/crossgray.svg";
 
-const Links = ({ link, onClick }) => {
+const Links = ({ link, onClick, isMobileMenuActive }) => {
   const { name, target } = link;
 
   const [isHovered, setIsHovered] = useState(false); // État pour gérer le survol
@@ -15,11 +13,23 @@ const Links = ({ link, onClick }) => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <a href={target} onClick={onClick}>
-        {name}
-      </a>
-      {isHovered && (
-        <img className={styles.__hoverImage} src={crossImage} alt="Cross" />
+      {isMobileMenuActive ? (
+        <a
+          href={target}
+          onClick={onClick}
+          style={{ color: isHovered ? "blue" : "white" }}
+        >
+          {name}
+        </a>
+      ) : (
+        <>
+          <a href={target} onClick={onClick}>
+            {name}
+          </a>
+          {!isMobileMenuActive && isHovered && (
+            <img className={styles.__hoverImage} src={crossImage} alt="Cross" />
+          )}
+        </>
       )}
     </li>
   );
