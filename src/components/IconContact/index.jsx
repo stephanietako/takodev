8bc41d97c07fdbdadeb0d20d300524dfed8b7b53
stephanietako <gabeshine@live.fr>
2023-10-05 19:15:43 +0200

    rework & refacto

--- src/components/IconContact/index.jsx
+++ src/components/IconContact/index.jsx
@@ -1,16 +1,11 @@
 // Assets
 import mail from "../../assets/icon/at-clear.png";
-// import linkedin from "../../assets/icon/linkedin.png";
+import linkedin from "../../assets/icon/linkedin.png";
 // Styles
 import styles from "./styles.module.scss";
 
 const IconContact = () => {
   return (
-    //     <div class="segment">
-    //   <button class="unit" type="button"><i class="icon ion-md-arrow-back"></i></button>
-    //   <button class="unit" type="button"><i class="icon ion-md-bookmark"></i></button>
-    //   <button class="unit" type="button"><i class="icon ion-md-settings"></i></button>
-    // </div>
     <div className={styles.__iconBox}>
       <a href="/">
         <button type="button" className={styles.unit}>
@@ -19,13 +14,12 @@ const IconContact = () => {
         </button>
       </a>
 
-      {/* <a href="/">
-        <img
-          className={styles.__icon_linkedin}
-          src={linkedin}
-          alt="icon linkedin"
-        />
-      </a> */}
+      <a href="/">
+        <button type="button" className={styles.unit}>
+          {" "}
+          <img className={styles.icon} src={linkedin} alt="icon likl" />
+        </button>
+      </a>
     </div>
   );
 };
