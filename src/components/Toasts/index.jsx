e97b6ed44c5409e2a7fefa8bfc26ce74dbd54047
stephanietako <gabeshine@live.fr>
2023-10-31 11:16:36 +0100

    refacto & add toasts messages

--- src/components/Toasts/index.jsx
+++ src/components/Toasts/index.jsx
@@ -14,22 +14,6 @@ const notify = (message) => {
     progress: undefined,
     theme: "dark",
   });
-
-  //   toast.success("Success Notification !", {
-  //     position: toast.POSITION.TOP_CENTER,
-  //   });
-
-  //   toast.error("Error Notification !", {
-  //     position: toast.POSITION.TOP_LEFT,
-  //   });
-
-  //   toast.warn("Warning Notification !", {
-  //     position: toast.POSITION.BOTTOM_LEFT,
-  //   });
-
-  //   toast.info("Info Notification !", {
-  //     position: toast.POSITION.BOTTOM_CENTER,
-  //   });
 };
 
 export { ToastContainer, notify, Zoom, toast };
